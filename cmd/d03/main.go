5f4077800bfc75d5dd98cd4d6e20fee8ba10e655
Jonathon Pinnow <jonathon.pinnow@stablekernel.com>
2022-12-04 23:53:11 -0500

    day three WIP

--- cmd/d03/main.go
+++ cmd/d03/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	"go_aoc/input"
 )
 
 /*
@@ -10,8 +12,9 @@ use math on the runes to calculate scores for each of the redundant items in the
 */
 
 func main() {
-	input := "vJrwpWtwJgWrhcsFMMfFFhFpjqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSLPmmdzqPrVvPwwTWBwgwMqvLMZHhHMvwLHjbvcjnnSBnvTQFnttgJtRGJQctTZtZTCrZsJsPPZsGzwwsLwLmpwMDw"
-	fmt.Printf("priority item sum: %d", ruck(input))
+	data := input.GetInputData("day3.txt")
+	in := strings.Join(data, "")
+	fmt.Printf("priority item sum: %d\n", ruck(in))
 }
 
 func ruck(input string) int {
@@ -24,27 +27,51 @@ func ruck(input string) int {
 		end    = 6
 		length = len(input)
 	)
+	// slice input into pocket sized chuncks
 	var pockets []string
 	for end <= length {
 		pockets = append(pockets, input[start:end])
 		start = end
 		end += pocketSize
 	}
+	// iterate over pocket sets
+	var pocketCount = len(pockets)
 	var rItems []rune
-	for i := 0; i < len(pockets); i += 2 {
-		if i+1 > len(pockets) {
+	for i := 0; i < pocketCount; i += 2 {
+		if i+1 > pocketCount {
 			break
 		}
-		if !strings.ContainsAny(pockets[i], pockets[i+1]) {
+		left := pockets[i]
+		right := pockets[i+1]
+		if !strings.ContainsAny(left, right) {
 			continue
 		}
-		for j := range pockets[i] {
-			if strings.ContainsRune(pockets[i+1], rune(pockets[i][j])) {
-				rItems = append(rItems, string(pockets[i][j]))
+		// iterate over a pocket
+		for j := range left {
+			if strings.ContainsRune(right, rune(left[j])) {
+				rItems = append(rItems, rune(left[j]))
 			}
 		}
 	}
-	fmt.Printf("%+v\n", rItems)
-	fmt.Printf("%v, %v\n", 'a'-96, 'A'-38)
+	//fmt.Printf("%+v\n", pockets)
+	//fmt.Printf("%+v\n", rItems)
+	fmt.Printf("a=%v, A=%v\n", 'a', 'A')
+	var result int
+	for i := range rItems {
+		result += score(rItems[i])
+	}
+	return result
+}
+
+func score(in rune) int {
+	const (
+		lowCaseOffset   = 96
+		upperCaseOffset = 64
+	)
+	if in > lowCaseOffset && in < lowCaseOffset+26 {
+		return int(in) - lowCaseOffset
+	} else if in > upperCaseOffset && in < upperCaseOffset+26 {
+		return int(in) - upperCaseOffset + 26
+	}
 	return 0
 }
--- cmd/d03/main_test.go
+++ cmd/d03/main_test.go
@@ -6,10 +6,23 @@ import (
 )
 
 const (
-	input       = `vJrwpWtwJgWrhcsFMMfFFhFpjqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSLPmmdzqPrVvPwwTWBwgwMqvLMZHhHMvwLHjbvcjnnSBnvTQFnttgJtRGJQctTZtZTCrZsJsPPZsGzwwsLwLmpwMDw`
+	data        = `vJrwpWtwJgWrhcsFMMfFFhFpjqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSLPmmdzqPrVvPwwTWBwgwMqvLMZHhHMvwLHjbvcjnnSBnvTQFnttgJtRGJQctTZtZTCrZsJsPPZsGzwwsLwLmpwMDw`
 	expectedSum = 157
 )
 
 func TestRuck(t *testing.T) {
-	assert.Equal(t, expectedSum, ruck(input))
+	assert.Equal(t, expectedSum, ruck(data))
+}
+
+func TestScore(t *testing.T) {
+	A := 'A'
+	a := 'a'
+	Z := 'Z'
+	z := 'z'
+	M := 'M'
+	assert.Equal(t, 27, score(A))
+	assert.Equal(t, 1, score(a))
+	assert.Equal(t, 27+25, score(Z))
+	assert.Equal(t, 26, score(z))
+	assert.Equal(t, 26, score(M))
 }
